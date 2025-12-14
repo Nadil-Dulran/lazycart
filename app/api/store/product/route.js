3c82a9769a180c67832ac75f523bdc19c18a22da
Nadil-Dulran <it23161788@my.sliit.lk>
2025-12-14 18:01:38 +0530

    Develop api to add new product

---
+++ app/api/store/product/route.js
@@ -0,0 +1,72 @@
+import imagekit from "@/configs/imageKit"
+import prisma  from "@/lib/prisma"
+import  NextResponse  from "next/server"
+import authSeller from "@/middlewares/authSeller"
+import {getAuth} from "@clerk/nextjs/server"
+
+
+
+// Add new product
+export async function POST(request) {
+    try{
+        const { userId } = getAuth(request)
+        const storeId = await authSeller(userId)
+
+        if(!storeId){
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+        }
+
+        // Get the data from form
+        const formData = await request.formData()
+        const name = formData.get('name')
+        const description = formData.get('description')
+        const mrp = Number(formData.get('mrp'))
+        const price = Number(formData.get('price'))
+        const category = formData.get('category')
+        const images = formData.getAll('images')
+
+        // Validate the data 
+        if(!name || !description || !mrp || !price || !category || images.length < 1){
+            return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+        }
+
+        // Uploading images to imagekit
+        const imageUrl = await Promise.all(images.map(async (image) => {
+            const buffer = Buffer.from(await image.arrayBuffer());
+            const response = await imagekit.upload({
+                file: buffer,
+                fileName: image.name,
+                folder: "products"
+            })
+
+            const url = imagekit.url({
+                path: response.filePath,
+                transformation: [
+                    {quality: "auto"},
+                    {format: "webp"},
+                    {width: "1024"}
+                ]
+            })
+            return url
+        }))
+
+        await prisma.product.create({
+            data: {
+                name,
+                description,
+                mrp,
+                price,
+                category,
+                images: imageUrl,
+                storeId
+            }
+        })
+
+        return NextResponse.json({ message: "Product added successfully" }, { status: 201 })
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error.code || error.message}, { status: 400 })
+
+    }
+}
