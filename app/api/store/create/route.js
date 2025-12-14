8e402c56b9e3aeba2d679c9cdd86abfb4e150ac7
Nadil-Dulran <it23161788@my.sliit.lk>
2025-12-14 17:21:47 +0530

    Develop api to create new store

---
+++ app/api/store/create/route.js
@@ -0,0 +1,94 @@
+import prisma from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+
+// Create the store
+export async function POST(req) {
+    try {
+        const { userId } = getAuth(request)
+        // Get the data from form
+        const formData = await request.formData()
+
+        const name = formData.get('name')
+        const username = formData.get('username')
+        const description = formData.get('description')
+        const email = formData.get('email')
+        const contact = formData.get('contact')
+        const address = formData.get('address')
+        const image = formData.get('image')
+
+        // Validate the data 
+        if(!name || !username || !description || !email || !contact || !address){
+            return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+        }
+
+        // Check if the user already has a store
+        const store = await prisma.store.findFirst({
+            where: {
+                userId: userId
+            }
+        })
+
+        // If the user already has a store, return an error
+        if(store){
+            return NextResponse.json({ status: store.status })
+        }
+
+        // Check if the username is already taken
+        const isUsernameTaken = await prisma.store.findFirst({
+            where: {
+                username: username.toLowerCase()
+            }
+        })
+
+        if(isUsernameTaken){
+            return NextResponse.json({ error: "Username is already taken" }, { status: 400 })
+        }
+
+        // Image upload to imagekit
+        const buffer = Buffer.from(await image.arrayBuffer());
+        const response = await imagekit.upload({
+            file: buffer,
+            fileName: image.name,
+            folder: "logos"
+        })
+
+        const optimizedImage= imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {quality: "auto"},
+                    {format: "webp"},
+                    {width: "512"}
+            ]
+        })
+
+        const newStore = await prisma.store.create({
+            data: {
+                userId,
+                name, 
+                description,
+                username: username.toLowerCase(),
+                email,
+                contact,
+                address,
+                logo: optimizedImage
+            }
+        })
+
+        // Link store to user
+        await prisma.user.update({
+            where: {id: userId},
+            data: { store: {connect: {id: newStore.id }} }
+        })
+
+        return NextResponse.json({ message: "Applied, wait for approval"})
+
+
+    }catch(error){
+        console.error(error);
+        return NextResponse.json({ error: error.code || error.message}, { status: 400 })
+
+    }
+}
