79ff18c6610af4d2b3b8ac9a217166c162cf204c
Nadil-Dulran <it23161788@my.sliit.lk>
2025-12-17 21:57:37 +0530

    Develop function access of store

--- app/api/store/is-seller/route.js
+++ app/api/store/is-seller/route.js
@@ -1,13 +1,13 @@
 import prisma from "@/lib/prisma";
 import authSeller from "@/middlewares/authSeller";
-import { getAuth } from "@clerk/nextjs/server"
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
 // Auth Seller
 export async function GET(request) {
     try{
-         const { userId } = getAuth(request)
+         const {userId} = getAuth(request)
         const isSeller = await authSeller(userId)
 
         if(!isSeller){
