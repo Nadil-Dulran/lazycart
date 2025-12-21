204a748f467b3949c4083f3e2ae8358166de71ab
Nadil-Dulran <it23161788@my.sliit.lk>
2025-12-21 19:59:02 +0530

    Update orders API

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,3 +1,4 @@
+import prisma from "@/lib/prisma";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -6,7 +7,44 @@ export async function POST(request) {
     try{
         const { userId, has} = getAuth(request)
         if(!userId){
-            return NextResponse
+            return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+        }
+        const { addressId, items, couponCode, paymentMethod } = await request.json()
+
+        // Check required fields are present
+        if (!addressId || !paymentMethod || !items || !Array.isArray(items) || items.length === 0) {
+            return NextResponse.json({ error: 'Missing required fields' }, { status: 400 })
+        }
+
+        // Ckech coupon
+        let coupon = null
+        if(couponCode){
+                   coupon = await prisma.coupon.findUnique({
+            where: {code: couponCode}
+        })
+        if(!coupon){
+            return NextResponse.json({error: 'Coupon not found'}, {status: 400})
+        }
+        }
+
+
+
+        if(coupon.forNewUser){
+            const userorders = await prisma.order.findMany({
+                where: {
+                    userId: userId
+                }
+            })
+            if(userorders.length > 0){
+                return NextResponse.json({error: 'Coupon valid for new users only'}, {status: 400})
+            }
+        }
+
+        if (coupon.forMember){
+            const hasPlusPlan = has({plan: 'plus'})
+            if(!hasPlusPlan){
+                return NextResponse.json({error: 'Coupon valid for Members only'}, {status: 400})
+            }
         }
     }catch(error){
         console.log('[ORDERS_POST]', error);
